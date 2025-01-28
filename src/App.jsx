af63b5ee88d2d8fe5f9ce8397522657a996ea579
MindOfBear <ovidiunoc@gmail.com>
2025-01-29 00:26:07 +0200

    Init project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex flex-row min-h-screen justify-center items-center'>
+        <p className='text-[30px]'>MindSpark</p>
+      </div>
+    </>
+  )
+}
+
+export default App
