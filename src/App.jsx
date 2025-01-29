5ea7a808a4bb516e91e6e743460b572e146af26f
MihaitaNicolaescu <Mihaita_nicolaescu@yahoo.com>
2025-01-30 00:54:49 +0200

    Worked on implementing app interface

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ function App() {
 
   return (
     <>
-      <div className='flex flex-row min-h-screen justify-center items-center'>
-        <p className='text-[30px]'>MindSpark</p>
+      <div className='flex flex-row min-h-screen justify-center items-center bg-[#5C7285]'>
+        <div className='grid grid-flow-row'>
+          <p className='text-[50px] text-[#E2E0C8] font-thin font-serif'>MindSpark</p>
+          <button className='rounded bg-[#A7B49E] w-full h-10 font-bold'>Start Quiz</button>
+        </div>
       </div>
     </>
   )
